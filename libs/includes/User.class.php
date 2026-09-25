ad6fd600f20961bbc5f2664ad6a68a88a70eb927
cibivalan <cibivalanarokiyaraj.gmail.com>
2026-09-25 03:13:32 -0400

    Sessions

--- libs/includes/User.class.php
+++ libs/includes/User.class.php
@@ -1,26 +1,32 @@
 <?php
-
 class User
 {
+    private $conn;
+
     public static function signup($user, $pass, $email, $phone)
     {
         $conn = Database::getDatabaseConnection();
-            $sql = "INSERT INTO `auth` (`username`, `password`, `email`, `phone`, `blocked`, `active`)
-            VALUES ('$user', '$pass', '$email', '$phone', '0', '1')";
+        $sql = "INSERT INTO `auth` (`username`, `password`, `email`, `phone`, `blocked`, `active`)
+                VALUES ('$user', '$pass', '$email', '$phone', '0', '1')";
 
-    $error = false;
-    if ($conn->query($sql) === TRUE) {
         $error = false;
-    } else {
-        // MySQL error 1062 = Duplicate entry (unique constraint violated)
-        if ($conn->errno === 1062) {
-            $error = "That username or email is already taken. Please choose a different one.";
+
+        if ($conn->query($sql) === TRUE) {
+            $error = false;
         } else {
-            $error = $conn->error;
+            if ($conn->errno === 1062) {
+                $error = "That username or email is already taken. Please choose a different one.";
+            } else {
+                $error = $conn->error;
+            }
         }
+
+        $conn->close();
+        return $error;
     }
 
-    $conn->close();
-    return $error;
+    public function __construct($username = null, $password = null, $email = null, $phone = null)
+    {
+        $this->conn = Database::getDatabaseConnection();
     }
 }
--- libs/load.php
+++ libs/load.php
@@ -1,15 +1,18 @@
 <?php
 
 include_once 'includes/Mic.class.php';
+include_once 'includes/Session.class.php';
 include_once 'includes/User.class.php';
 include_once 'includes/Database.class.php';
+
+Session::start();
 
 function load_template($name){
     include $_SERVER['DOCUMENT_ROOT']."/htdocs/app/__templates/$name.php";
 }
 
 function validate_credentials($username, $password) {
-    if($username == "jeromcibivalan@gmail.com" and $password == "password"){
+    if($username == "jeromcibi@gmail.com" and $password == "password"){
         return true;
     } else {
         return false;
